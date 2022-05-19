ef00786011cfc1a055017885bafa57cecabaa947
Anirban <ani.b@tuta.io>
2022-05-19 20:40:07 +0530

    Major code fixes

--- js/main.js
+++ js/main.js
@@ -1,126 +1,116 @@
-let newDeckBtn = document.querySelector('.btnDeck')
-let resetDeck = document.querySelector(`.btnReset`)
-let drawCardBtn = document.querySelector(`.drawCards`)
-newDeckBtn.addEventListener('click', fetchDeck)
-let winner
+document.querySelector('.btnDeck').addEventListener('click', fetchDeck)
 
 function fetchDeck() {
     fetch(`https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
     .then(res => res.json())
-    .then(data =>createDecks(data))
+    .then(data =>{
+        let deckId = data.deck_id
+        resetDOM()
+        createTwoDecks(deckId)
+        document.querySelector(`.btnReset`).addEventListener('click' , () => {
+            resetDOM()
+            fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/return/`)
+        })
+    })
     .catch(err => console.log(err))
 }
-
-function createDecks(data) {
-    let deckOne =[], deckTwo = [];
-    let deckId = data.deck_id
+function resetDOM() {
+    document.querySelector(`.result`).innerText = ''
+    let spanList = document.querySelectorAll('span')
+    let imgList = document.querySelectorAll('img')
+    let btnList = document.querySelectorAll('button')
+    Array.from(imgList).forEach(e => e.src ='')
+    Array.from(spanList).forEach(e => e.innerText = '')
+    Array.from(btnList).forEach(e => e.removeAttribute('disabled'))
+}
+function createTwoDecks(deckId) {
+    let deckOne =[], deckTwo =[]
     fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`)
     .then(res => res.json())
-    .then(data => populateDecks(data))
-
-    resetDeck.addEventListener('click', () => {
-        winner = undefined
-        let imgList = document.querySelectorAll('img')
-        Array.from(imgList).forEach(e => e.src = '')
-        document.querySelector('.result').innerText = ''
-        fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/shuffle/`)
-
-        fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`)
-        .then(res => res.json())
-        .then(data => populateDecks(data))
-    })
-
-    function populateDecks(data) {
-        
-        for(let i = 0; i<data.cards.length; i++) {
-            if(i%2 === 0) deckOne.push(data.cards[i])
-            else deckTwo.push(data.cards[i])
-        }
-        letsPlay(deckOne, deckTwo)
+    .then(data => {
+        console.log(data)
+        for(let i = 0; i<data.cards.length; i=i+2) {
+            deckOne.push(data.cards[i])
+            deckTwo.push(data.cards[i+1])
     }
-}
-
-
-function letsPlay(deckOne, deckTwo) {
-    if(winner == undefined) {
-
-        let discardOneDeck = []
-        let discardTwoDeck = []
-        let warStakes = []
-        let cardOne, cardTwo,valOne,valTwo
-        const result = document.querySelector('.result')
-
-        drawCardBtn.addEventListener('click', drawCards)
-        
-        function drawCards() {
-            if(deckOne.length > 0 && deckTwo.length > 0) {
-
-                cardOne = deckOne.pop()
-                warStakes.push(cardOne)
-                document.querySelector(`.cardOne`).src = cardOne.image
-        
-                cardTwo = deckTwo.pop()
-                warStakes.push(cardTwo)
-                document.querySelector(`.cardTwo`).src = cardTwo.image
-        
-                valOne = getCardVal(cardOne)
-                valTwo = getCardVal(cardTwo)
-        
-                checkWin(valOne,valTwo)
-            } else combineDeck()
-
-        }
-
-        function checkWin(valOne,valTwo) {
-
-            if(valOne > valTwo) {
-                discardOneDeck = discardOneDeck.concat(warStakes)
-                warStakes =[]
-                result.innerText = `Player 1 wins this round! Draw Again!`
-            } else if( valTwo > valOne) {
-                discardTwoDeck = discardTwoDeck.concat(warStakes)
-                warStakes = []
-                result.innerText = `Player 2 wins this round! Draw Again!`
-            } else if(valOne === valTwo) {
-                result.innerText = `It's WAR!`
-                initiateWar()
-            }
-        }
-
-        function initiateWar() {
-
-            let totalCardsOne = deckOne.length + discardOneDeck.length
-            let totalCardsTwo = deckTwo.length + discardTwoDeck.length
-
-            if(totalCardsOne > 3 && totalCardsTwo > 3) {
-                for(let i = 0; i < 3; i++) {
-                    warStakes.push(deckOne.pop())
-                    warStakes.push(deckTwo.pop())
-                }
-                drawCards()
-            } else gameOver(totalCardsOne,totalCardsTwo)
-            
-        }
-
-        function combineDeck() {
-            deckOne = shuffleDeck(deckOne.concat(discardOneDeck))
-            deckTwo = shuffleDeck(deckTwo.concat(discardTwoDeck))
-            discardOneDeck = []
-            discardTwoDeck = []
-            if(deckOne.length === 0 || deckTwo.length === 0) {
-                gameOver(deckOne.length, deckTwo.length)
-            }
-        }
-    }
-}
-
-function gameOver(l1,l2) {
     
-    l1 > l2 ? winner = `1` : winner = '2'
-    fetchDeck()
-    document.querySelector('.result').innerText = `Player ${winner} has won the game. Please Reset or fetch a new Deck.`
+    playGame(deckOne,deckTwo)
+})
 }
+function playGame(deckOne,deckTwo) {
+    let result = document.querySelector('.result')
+    let cardOne, cardTwo, valOne, valTwo, totalCardOne, totalCardTwo, stakes = [], tempOneDeck =[], tempTwoDeck =[];
 
+    document.querySelector(`.drawCards`).addEventListener('click', () => {
+        totalCardOne = deckOne.length + tempOneDeck.length
+        totalCardTwo = deckTwo.length + tempTwoDeck.length
+        
+        if(deckOne.length === 0 || deckTwo.length === 0) mergeDecks()
+
+        cardOne = deckOne.pop()
+        cardTwo = deckTwo.pop()
+        stakes.push(cardOne,cardTwo)
+
+        displayDOM()
+        checkWin()
+    })
+    function checkWin() {
+
+        valOne = checkVal(cardOne)
+        valTwo = checkVal(cardTwo)
+
+        totalCardOne = deckOne.length + tempOneDeck
+        totalCardTwo = deckTwo.length + tempTwoDeck.length
+    
+        if(valOne > valTwo) {
+            tempOneDeck =tempOneDeck.concat(stakes)
+            stakes=[]
+            result.innerText = `Player One wins this round!`
+        }
+        else if(valOne < valTwo) {
+            tempTwoDeck = tempTwoDeck.concat(stakes)
+            stakes =[]
+            result.innerText = `Player Two wins this round!`
+        }
+        else {
+            if(totalCardOne < 4 || totalCardTwo < 4) {
+                totalCardOne = deckOne.length + tempOneDeck
+                totalCardTwo = deckTwo.length + tempTwoDeck.length
+
+                gameOver(totalCardOne,totalCardTwo)
+
+            } else war()
+        }
+    }
+    function displayDOM() {
+        document.querySelector(`.deckValOne`).innerText = deckOne.length
+        document.querySelector(`.deckValTwo`).innerText = deckTwo.length
+
+        document.querySelector(`.deckOne`).src = cardOne.image
+        document.querySelector(`.deckTwo`).src = cardTwo.image
+
+        document.querySelector(`.discardValOne`).innerText = tempOneDeck.length
+        document.querySelector(`.discardValTwo`).innerText = tempTwoDeck.length  
+        
+        document.querySelector('.warStakes').innerText = stakes.length
+    }
+    function war() {
+        for(let i = 0; i<3; i++){
+            stakes.push(deckOne.pop(), deckTwo.pop())
+        }
+        displayDOM()
+        result.innerText = `It's WAR! 3 cards more at stake from each player`
+    }
+    function mergeDecks() {
+        deckOne = shuffleDeck(deckOne.concat(tempOneDeck))
+        tempOneDeck = []
+
+        deckTwo = shuffleDeck(deckTwo.concat(tempTwoDeck))
+        tempTwoDeck = []
+        
+        if(deckOne.length === 0 || deckTwo.length === 0) gameOver(deckOne.length, deckTwo.length)
+    }
+}
 function shuffleDeck(a) {
     let j, x, i;
     for (i = a.length - 1; i > 0; i--) {
@@ -129,19 +119,23 @@ function shuffleDeck(a) {
         a[i] = a[j];
         a[j] = x;
     }
-    return a
+    return a;
 }
-
-function getCardVal(card){
-    
+function gameOver(score1,score2) {
+    let winner
+    score1 > score2 ? winner = 1 : winner = 2
+    document.querySelector(`.drawCards`).setAttribute('disabled', true)
+    document.querySelector('.result').innerText = `Player ${winner} has Won the game! Wohoo!` 
+}
+function checkVal(card) {
     switch(card.value) {
-        case 'ACE' : return 14
+        case 'KING': return 13
         break;
-        case 'KING' : return 13
+        case 'QUEEN': return 12
         break;
-        case 'QUEEN' : return 12
+        case 'JACK': return 11
         break;
-        case 'JACK' : return 11
+        case 'ACE': return 14
         break;
         default: return Number(card.value)
         break;
